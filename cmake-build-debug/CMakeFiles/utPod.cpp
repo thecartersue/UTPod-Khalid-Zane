b0aeffb97f91739aa5cc6a636a0bd472bac9717b
KhalidAhmad95 <khalid.ashraf.ahmad@gmail.com>
2018-10-31 15:12:03 -0500

    testing git

--- cmake-build-debug/CMakeFiles/utPod.cpp
+++ cmake-build-debug/CMakeFiles/utPod.cpp
@@ -7,7 +7,6 @@ using namespace std;
 UtPod::UtPod(){
     songs = nullptr;
     memSize = MAX_MEMORY;
-    //testing github
 }
 
 UtPod::UtPod(int size){
@@ -17,11 +16,11 @@ UtPod::UtPod(int size){
 }
 
 int UtPod::addSong(Song const &s) {
-    if (s.getArtist() == "" || s.getTitle() == "" || s.getSize() < 1) {
+    if (s.getArtist(s) == "" || s.getTitle(s) == "" || s.getSize(s) < 1) {
         return -1;
     } else {
-        if (memSize - s.getSize() >= 0) {
-            memSize = memSize - s.getSize();
+        if (memSize - s.getSize(s) >= 0) {
+            memSize = memSize - s.getSize(s);
             SongNode *temp = songs;
             temp = new SongNode;
             temp->s = s;
@@ -42,8 +41,21 @@ int UtPod::addSong(Song const &s) {
 }
 
 int UtPod::removeSong(Song const &s) {
+    SongNode *findSong = songs;
+    SongNode *trail = songs;
+    while(findSong->s.getTitle(s) != s.getTitle(s) && findSong->s.getArtist(s) != s.getArtist(s) && findSong->s.getSize(s) != s.getSize(s)) {
+        trail = findSong;
+        if (findSong->next != nullptr) {
+            findSong = findSong->next;
+        }else return -1;
+    }
+    //readjusting the ll
+    trail->next= findSong->next;
+    //readjust memory
+    memSize = memSize + s.getSize(s);
+    //delete node
+    delete findSong;
     return 0;
-
 }
 
 void UtPod::shuffle() {
@@ -51,11 +63,48 @@ void UtPod::shuffle() {
 }
 
 void UtPod::showSongList() {
-
+    SongNode *songList = songs;
+    while(songList != nullptr) {
+        cout << songList->s.getTitle(songList->s) << ", " << songList->s.getArtist(songList->s) << ", " << songList->s.getSize(songList->s) << "\n";
+        songList = songList->next;
+    }
 }
 
 void UtPod::sortSongList() {
+    SongNode *tempSort = songs;
+    string tempArtist, tempTitle;
+    int counter = 0, tempSize = 0;
+    //get number of songs
+    while(tempSort!= nullptr){
+        tempSort = tempSort->next;
+        counter++;
+    }
+    tempSort = songs;
+    for(int j = 0; j < counter ; j++){
+        while(tempSort->next != nullptr){
+            if(tempSort->s > tempSort->next->s){
+                tempArtist = tempSort->s.getArtist(tempSort->s);
+                tempTitle = tempSort->s.getTitle(tempSort->s);
+                tempSize = tempSort->s.getSize(tempSort->s);
 
+                //change the trailing node
+                tempSort->s.setArtist(tempSort->next->s.getArtist(tempSort->next->s));
+                tempSort->s.setTitle(tempSort->next->s.getTitle(tempSort->next->s));
+                tempSort->s.setSize(tempSort->next->s.getSize(tempSort->next->s));
+
+                //change the first node
+                tempSort->next->s.setArtist(tempArtist);
+                tempSort->next->s.setTitle(tempTitle);
+                tempSort->next->s.setSize(tempSize);
+
+                tempSort = tempSort->next;
+            }else{
+                tempSort = tempSort->next;
+            }
+        }
+        //repeat
+        tempSort = songs;
+    }
 }
 
 void UtPod::clearMemory() {
